1b1aa0b44dd32a38e92b537f4963096f76354861
cyberfedya <firdavsyuldashov03@gmail.com>
2026-05-18 13:09:34 +0500

    update

--- query.sql
+++
@@ -1,5 +0,0 @@
-SELECT f."Id", f."Name", fm."UserId", fm."Role", fm."Status", u."Email"
-FROM "Families" f
-LEFT JOIN "FamilyMembers" fm ON fm."FamilyId" = f."Id"
-LEFT JOIN "AspNetUsers" u ON u."Id" = fm."UserId"
-ORDER BY f."Id";
